a49caae840fd4ca9f31b2d371527399d42e60b91
Isaac Antonio Gutierrez <Inggutierrez1113@hotmail.com>
2017-02-02 20:14:20 -0400

    codigo fuente de la aplicacion wed

---
+++ SearchMyHome.API/SearchMyHome.API/Controllers/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace SearchMyHome.API.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult GetImage() {
+
+                byte[] fileBytes = System.IO.File.ReadAllBytes(@"C:\Users\Isaac Gutierrez\Downloads\ForRentHome-1050x751.jpg");
+                string filename = "ForRentHome-1050x751.jpg";
+                return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filename);        
+        }
+    }
+}
